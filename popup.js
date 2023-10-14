6870d25de68d900589f4b054687f9cda685ec80b
AJ <adamjch@proton.me>
2023-10-14 04:22:24 +0100

    v1.1
    two clocks to 24 hr format
    updated styles, broke style
    added background script, broke time zone & healing spring timer

--- popup.js
+++ popup.js
@@ -1,42 +1,25 @@
-// Function to update NST and GMT times
-function updateTime() {
-    const nstTimeElement = document.getElementById("nstTime");
-    const gmtTimeElement = document.getElementById("gmtTime");
-  
-    // Get current time in U.S. Pacific Time (Neopets Time)
-    const nstDate = new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" });
-    nstTimeElement.innerText = nstDate;
-  
-    // Get current time in GMT
-    const gmtDate = new Date().toLocaleString("en-US", { timeZone: "GMT" });
-    gmtTimeElement.innerText = gmtDate;
+function updateClocks() {
+    const now = new Date();
+    
+    // Neopets Time (U.S. Pacific Time)
+    const neopetsTime = new Date(now.toLocaleString("en-US", { timeZone: "America/Los_Angeles" }));
+    const neopetsHours = neopetsTime.getHours().toString().padStart(2, '0');
+    const neopetsMinutes = neopetsTime.getMinutes().toString().padStart(2, '0');
+    document.getElementById("neopetsTime").innerText = `${neopetsHours}:${neopetsMinutes}`;
+    
+    // GMT
+    const gmtHours = now.getUTCHours().toString().padStart(2, '0');
+    const gmtMinutes = now.getUTCMinutes().toString().padStart(2, '0');
+    document.getElementById("gmtTime").innerText = `${gmtHours}:${gmtMinutes}`;
   }
   
-  // Update time initially and then every second
-  updateTime();
-  setInterval(updateTime, 1000);
+  // Update the clocks every minute
+  setInterval(updateClocks, 60000);
+  
+  // Update the clocks immediately
+  updateClocks();  
 
-// Initialize Healing Springs Timer
-let healingTimer = null;
-let healingSeconds = 1800;
-
-function startHealingTimer() {
-  if (healingTimer) {
-    clearInterval(healingTimer);
-  }
-
-  healingTimer = setInterval(() => {
-    healingSeconds--;
-    const minutes = Math.floor(healingSeconds / 60);
-    const seconds = healingSeconds % 60;
-    document.getElementById("healingTimer").textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
-
-    if (healingSeconds <= 0) {
-      clearInterval(healingTimer);
-      alert("Time to visit the Healing Springs!");
-      healingSeconds = 1800;
-    }
-  }, 1000);
-}
-
-startHealingTimer();
+  //Healing Springs timer
+  document.getElementById("healingSprings").addEventListener('click', () => {
+    chrome.runtime.sendMessage({ resetHealingTimer: true });
+  });
